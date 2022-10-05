bf1e6a5dc3fee724ddbac724f61f5e3fed5d445f
Brandon Tetrick <btetrick@Brandons-MacBook-Pro.local>
2022-10-05 08:23:37 -0600

    search rebate page update

--- pages/search-rebate.php
+++ pages/search-rebate.php
@@ -59,7 +59,21 @@ get_header();
 		<!-- #Widget Area After -->
 
 		<!-- Additional Content -->
-		<div id='fp_ug_widget_container' data-widget-token='0372c42f-d9ae-4be1-8756-01d062b88ff5' style="margin-top:50px;margin-bottom:100px;"></div> 
+		<div class="container"> 
+			<div id='fp_ug_widget_container' data-widget-token='0372c42f-d9ae-4be1-8756-01d062b88ff5' style="margin-top:50px;margin-bottom:25px;"></div> 
+			<p style="margin-left:15px;margin-right:15px;">Let APPI Energy help your business secure the best energy efficiency rebates. 
+				Our team goes to task for you, working with your local utility to capture 
+				rebates and ensure the process is a seamless as possible on your end. In many cases, 
+				APPI energy can provide the rebate to our clients up front, collecting 
+				it from the utility after the project is complete, 
+				significantly reducing the risk and wait time for our clients.
+			</p>
+			<p style="margin-left:15px;margin-right:15px;margin-bottom:50px;">To learn more, or to get started on the programs that your search (above) yielded, 
+				connect with us here via the chat box, <a href="/contact/" target="_blank">our website contact form</a>, 
+				or by calling <a href="tel:18005206685">800-520-6685 </a>.
+			</p>
+
+		</div>
 		<!-- #Additional Content -->
 
 	</main><!-- #main -->
